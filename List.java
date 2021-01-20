447ee635573cc532174f7ad1ddbd6e9a43206d58
Hgamo <73846452+Hgamo@users.noreply.github.com>
2021-01-20 14:36:17 +0100

    Verbesserung der Kommentierung

--- List.java
+++ List.java
@@ -1,4 +1,4 @@
- /**
+/**
  * <p>
  * Materialien zu den zentralen NRW-Abiturpruefungen im Fach Informatik ab 2018
  * </p>
@@ -10,7 +10,8 @@
  * angeordnete Objekte vom Typ ContentType. Auf hoechstens ein Listenobjekt,
  * aktuellesObjekt genannt, kann jeweils zugegriffen werden.<br />
  * Wenn eine Liste leer ist, vollstaendig durchlaufen wurde oder das aktuelle
- * Objekt am Ende der Liste geloescht wurde, gibt es kein aktuelles Objekt.<br />
+ * Objekt am Ende der Liste geloescht wurde, gibt es kein aktuelles
+ * Objekt.<br />
  * Das erste oder das letzte Objekt einer Liste koennen durch einen Auftrag zum
  * aktuellen Objekt gemacht werden. Ausserdem kann das dem aktuellen Objekt
  * folgende Listenobjekt zum neuen aktuellen Objekt werden. <br />
@@ -68,8 +69,7 @@ public class List<ContentType> {
     }
 
     /**
-     * Der Verweis wird auf das Objekt, das als Parameter uebergeben
-     * wird, gesetzt.
+     * Der Verweis wird auf das Objekt, das als Parameter uebergeben wird, gesetzt.
      * 
      * @param pNext der Nachfolger des Knotens
      */
@@ -111,22 +111,21 @@ public class List<ContentType> {
   }
 
   /**
-   * Die Anfrage liefert den Wert true, wenn es ein aktuelles Objekt gibt,
-   * sonst liefert sie den Wert false.
+   * Die Anfrage liefert den Wert true, wenn es ein aktuelles Objekt gibt, sonst
+   * liefert sie den Wert false.
    * 
    * @return true, falls Zugriff moeglich, sonst false
    */
   public boolean hasAccess() {
     // Es gibt keinen Zugriff, wenn current auf kein Element verweist.
-    return current != null; 
+    return current != null;
   }
 
   /**
-   * Falls die Liste nicht leer ist, es ein aktuelles Objekt gibt und dieses
-   * nicht das letzte Objekt der Liste ist, wird das dem aktuellen Objekt in
-   * der Liste folgende Objekt zum aktuellen Objekt, andernfalls gibt es nach
-   * Ausfuehrung des Auftrags kein aktuelles Objekt, d.h. hasAccess() liefert
-   * den Wert false.
+   * Falls die Liste nicht leer ist, es ein aktuelles Objekt gibt und dieses nicht
+   * das letzte Objekt der Liste ist, wird das dem aktuellen Objekt in der Liste
+   * folgende Objekt zum aktuellen Objekt, andernfalls gibt es nach Ausfuehrung
+   * des Auftrags kein aktuelles Objekt, d.h. hasAccess() liefert den Wert false.
    */
   public void next() {
     if (this.hasAccess()) {
@@ -145,8 +144,8 @@ public class List<ContentType> {
   }
 
   /**
-   * Falls die Liste nicht leer ist, wird das letzte Objekt der Liste
-   * aktuelles Objekt. Ist die Liste leer, geschieht nichts.
+   * Falls die Liste nicht leer ist, wird das letzte Objekt der Liste aktuelles
+   * Objekt. Ist die Liste leer, geschieht nichts.
    */
   public void toLast() {
     if (!isEmpty()) {
@@ -155,12 +154,12 @@ public class List<ContentType> {
   }
 
   /**
-   * Falls es ein aktuelles Objekt gibt (hasAccess() == true), wird das
-   * aktuelle Objekt zurueckgegeben, andernfalls (hasAccess() == false) gibt
-   * die Anfrage den Wert null zurueck.
+   * Falls es ein aktuelles Objekt gibt (hasAccess() == true), wird das aktuelle
+   * Objekt zurueckgegeben, andernfalls (hasAccess() == false) gibt die Anfrage
+   * den Wert null zurueck.
    * 
-   * @return das aktuelle Objekt (vom Typ ContentType) oder null, wenn es
-   *         kein aktuelles Objekt gibt
+   * @return das aktuelle Objekt (vom Typ ContentType) oder null, wenn es kein
+   *         aktuelles Objekt gibt
    */
   public ContentType getContent() {
     if (this.hasAccess()) {
@@ -175,34 +174,32 @@ public class List<ContentType> {
    * ungleich null ist, wird das aktuelle Objekt durch pContent ersetzt. Sonst
    * geschieht nichts.
    * 
-   * @param pContent
-   *            das zu schreibende Objekt vom Typ ContentType
+   * @param pContent das zu schreibende Objekt vom Typ ContentType
    */
   public void setContent(ContentType pContent) {
     // Nichts tun, wenn es keinen Inhalt oder kein aktuelles Element gibt.
-    if (pContent != null && this.hasAccess()) { 
+    if (pContent != null && this.hasAccess()) {
       current.setContentObject(pContent);
     }
   }
 
   /**
    * Falls es ein aktuelles Objekt gibt (hasAccess() == true), wird ein neues
-   * Objekt vor dem aktuellen Objekt in die Liste eingefuegt. Das aktuelle
-   * Objekt bleibt unveraendert. <br />
-   * Wenn die Liste leer ist, wird pContent in die Liste eingefuegt und es
-   * gibt weiterhin kein aktuelles Objekt (hasAccess() == false). <br />
+   * Objekt vor dem aktuellen Objekt in die Liste eingefuegt. Das aktuelle Objekt
+   * bleibt unveraendert. <br />
+   * Wenn die Liste leer ist, wird pContent in die Liste eingefuegt und es gibt
+   * weiterhin kein aktuelles Objekt (hasAccess() == false). <br />
    * Falls es kein aktuelles Objekt gibt (hasAccess() == false) und die Liste
    * nicht leer ist oder pContent gleich null ist, geschieht nichts.
    * 
-   * @param pContent
-   *            das einzufuegende Objekt vom Typ ContentType
+   * @param pContent das einzufuegende Objekt vom Typ ContentType
    */
   public void insert(ContentType pContent) {
     if (pContent != null) { // Nichts tun, wenn es keinen Inhalt gibt.
       if (this.hasAccess()) { // Fall: Es gibt ein aktuelles Element.
 
         // Neuen Knoten erstellen.
-        ListNode newNode = new ListNode(pContent); 
+        ListNode newNode = new ListNode(pContent);
 
         if (current != first) { // Fall: Nicht an erster Stelle einfuegen.
           ListNode previous = this.getPrevious(current);
@@ -213,12 +210,12 @@ public class List<ContentType> {
           first = newNode;
         }
 
-      } else { //Fall: Es gibt kein aktuelles Element.
+      } else { // Fall: Es gibt kein aktuelles Element.
 
         if (this.isEmpty()) { // Fall: In leere Liste einfuegen.
 
           // Neuen Knoten erstellen.
-          ListNode newNode = new ListNode(pContent); 
+          ListNode newNode = new ListNode(pContent);
 
           first = newNode;
           last = newNode;
@@ -230,13 +227,12 @@ public class List<ContentType> {
 
   /**
    * Falls pContent gleich null ist, geschieht nichts.<br />
-   * Ansonsten wird ein neues Objekt pContent am Ende der Liste eingefuegt.
-   * Das aktuelle Objekt bleibt unveraendert. <br />
-   * Wenn die Liste leer ist, wird das Objekt pContent in die Liste eingefuegt
-   * und es gibt weiterhin kein aktuelles Objekt (hasAccess() == false).
+   * Ansonsten wird ein neues Objekt pContent am Ende der Liste eingefuegt. Das
+   * aktuelle Objekt bleibt unveraendert. <br />
+   * Wenn die Liste leer ist, wird das Objekt pContent in die Liste eingefuegt und
+   * es gibt weiterhin kein aktuelles Objekt (hasAccess() == false).
    * 
-   * @param pContent
-   *            das anzuhaengende Objekt vom Typ ContentType
+   * @param pContent das anzuhaengende Objekt vom Typ ContentType
    */
   public void append(ContentType pContent) {
     if (pContent != null) { // Nichts tun, wenn es keine Inhalt gibt.
@@ -246,7 +242,7 @@ public class List<ContentType> {
       } else { // Fall: An nicht-leere Liste anfuegen.
 
         // Neuen Knoten erstellen.
-        ListNode newNode = new ListNode(pContent); 
+        ListNode newNode = new ListNode(pContent);
 
         last.setNextNode(newNode);
         last = newNode; // Letzten Knoten aktualisieren.
@@ -256,18 +252,17 @@ public class List<ContentType> {
   }
 
   /**
-   * Falls es sich bei der Liste und pList um dasselbe Objekt handelt,
-   * pList null oder eine leere Liste ist, geschieht nichts.<br />
+   * Falls es sich bei der Liste und pList um dasselbe Objekt handelt, pList null
+   * oder eine leere Liste ist, geschieht nichts.<br />
    * Ansonsten wird die Liste pList an die aktuelle Liste angehaengt.
    * Anschliessend wird pList eine leere Liste. Das aktuelle Objekt bleibt
    * unveraendert. Insbesondere bleibt hasAccess identisch.
    * 
-   * @param pList
-   *            die am Ende anzuhaengende Liste vom Typ List<ContentType>
+   * @param pList die am Ende anzuhaengende Liste vom Typ List<ContentType>
    */
   public void concat(List<ContentType> pList) {
     if (pList != this && pList != null && !pList.isEmpty()) { // Nichts tun,
-    // wenn pList und this identisch, pList leer oder nicht existent.
+      // wenn pList und this identisch, pList leer oder nicht existent.
 
       if (this.isEmpty()) { // Fall: An leere Liste anfuegen.
         this.first = pList.first;
@@ -285,17 +280,17 @@ public class List<ContentType> {
   }
 
   /**
-   * Wenn die Liste leer ist oder es kein aktuelles Objekt gibt (hasAccess()
-   * == false), geschieht nichts.<br />
-   * Falls es ein aktuelles Objekt gibt (hasAccess() == true), wird das
-   * aktuelle Objekt geloescht und das Objekt hinter dem geloeschten Objekt
-   * wird zum aktuellen Objekt. <br />
+   * Wenn die Liste leer ist oder es kein aktuelles Objekt gibt (hasAccess() ==
+   * false), geschieht nichts.<br />
+   * Falls es ein aktuelles Objekt gibt (hasAccess() == true), wird das aktuelle
+   * Objekt geloescht und das Objekt hinter dem geloeschten Objekt wird zum
+   * aktuellen Objekt. <br />
    * Wird das Objekt, das am Ende der Liste steht, geloescht, gibt es kein
    * aktuelles Objekt mehr.
    */
   public void remove() {
     // Nichts tun, wenn es kein aktuelle Element gibt oder die Liste leer ist.
-    if (this.hasAccess() && !this.isEmpty()) { 
+    if (this.hasAccess() && !this.isEmpty()) {
 
       if (current == first) {
         first = first.getNextNode();
@@ -312,7 +307,7 @@ public class List<ContentType> {
       current.setNextNode(null);
       current = temp;
 
-      //Beim loeschen des letzten Elements last auf null setzen. 
+      // Beim loeschen des letzten Elements last auf null setzen.
       if (this.isEmpty()) {
         last = null;
       }
@@ -320,15 +315,14 @@ public class List<ContentType> {
   }
 
   /**
-   * Liefert den Vorgaengerknoten des Knotens pNode. Ist die Liste leer, pNode
-   * == null, pNode nicht in der Liste oder pNode der erste Knoten der Liste,
-   * wird null zurueckgegeben.
+   * Liefert den Vorgaengerknoten des Knotens pNode. Ist die Liste leer, pNode ==
+   * null, pNode nicht in der Liste oder pNode der erste Knoten der Liste, wird
+   * null zurueckgegeben.
    *
-   * @param pNode
-   *         der Knoten, dessen Vorgaenger zurueckgegeben werden soll
+   * @param pNode der Knoten, dessen Vorgaenger zurueckgegeben werden soll
    * @return der Vorgaenger des Knotens pNode oder null, falls die Liste leer ist,
-   *         pNode == null ist, pNode nicht in der Liste ist oder pNode der erste Knoten
-   *         der Liste ist
+   *         pNode == null ist, pNode nicht in der Liste ist oder pNode der erste
+   *         Knoten der Liste ist
    */
   private ListNode getPrevious(ListNode pNode) {
     if (pNode != null && pNode != first && !this.isEmpty()) {
@@ -341,5 +335,5 @@ public class List<ContentType> {
       return null;
     }
   }
-  
+
 }
--- Test.java
+++ Test.java
@@ -1,20 +1,20 @@
 import java.time.LocalDateTime;
 
 public class Test {
-    Test() {
-    }
+        Test() {
+        }
 
-    public static void main(String[] args) {
-        Feed feed = new Feed();
-        feed.addFeedItem(LocalDateTime.now(),
-                "https://www.viktoriaschule-aachen.de/wp-content/uploads/2020/08/81724947_2742373012508692_7114063905017561088_n__1_-removebg-preview-300x300.png");
-        feed.addFeedItem(LocalDateTime.now(),
-                "https://www.viktoriaschule-aachen.de/wp-content/uploads/2020/08/81724947_2742373012508692_7114063905017561088_n__1_-removebg-preview-300x300.png");
-        feed.addFeedItem(LocalDateTime.now(),
-                "https://www.viktoriaschule-aachen.de/wp-content/uploads/2020/08/81724947_2742373012508692_7114063905017561088_n__1_-removebg-preview-300x300.png");
-        feed.addFeedItem(LocalDateTime.now(),
-                "https://www.viktoriaschule-aachen.de/wp-content/uploads/2020/08/81724947_2742373012508692_7114063905017561088_n__1_-removebg-preview-300x300.png");
-        feed.printOutputLong();
-    }
-    
+        public static void main(String[] args) {
+                Feed feed = new Feed();
+                feed.addFeedItem(LocalDateTime.now(),
+                                "https://www.viktoriaschule-aachen.de/wp-content/uploads/2020/08/81724947_2742373012508692_7114063905017561088_n__1_-removebg-preview-300x300.png");
+                feed.addFeedItem(LocalDateTime.now(),
+                                "https://www.viktoriaschule-aachen.de/wp-content/uploads/2020/08/81724947_2742373012508692_7114063905017561088_n__1_-removebg-preview-300x300.png");
+                feed.addFeedItem(LocalDateTime.now(),
+                                "https://www.viktoriaschule-aachen.de/wp-content/uploads/2020/08/81724947_2742373012508692_7114063905017561088_n__1_-removebg-preview-300x300.png");
+                feed.addFeedItem(LocalDateTime.now(),
+                                "https://www.viktoriaschule-aachen.de/wp-content/uploads/2020/08/81724947_2742373012508692_7114063905017561088_n__1_-removebg-preview-300x300.png");
+                feed.printOutputLong();
+        }
+
 }
